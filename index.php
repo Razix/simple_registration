584bfde5609fbd97ac027c5955c423411566eb96
Bogdan RBM <uscreamrazix@gmail.com>
2012-01-26 16:15:31 +0200

    sixth commit

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+<title>Main page</title>
+</head>
+
+<body>
+
 <?php
 
 session_start();
 
 include 'db.php';
+include_once 'header.php';
 
 if(isset($_SESSION['username']))
 {
-    echo 'Hello, '.'<b>'.$_SESSION['username'].'</b>'.'.';
     include 'logout.php';
-    echo '<b>'."Now it's time to add <a href='new_article.php'>
+    echo "<div id='content'>";
+    echo 'Hello, '.'<b>'.$_SESSION['username'].'</b>'.'.';
+    echo '<br>'.'<br>'.'<b>'."Now it's time to add <a href='new_article.php'>
     new article</a>.".'</b>'.'<br>';
     
     $queryuser = mysql_query("SELECT * FROM registration WHERE 
@@ -24,8 +36,14 @@ if(isset($_SESSION['username']))
 
 else
 {
-    include('login.php');
+    echo "<div id='content'>";
+    include('login_form.php');
 }
 
 ?>
-<b>Do u want to read something? Check <a href='articles.php'>this</a> out.</b>
+
+</div>
+
+</body>
+
+</html>
